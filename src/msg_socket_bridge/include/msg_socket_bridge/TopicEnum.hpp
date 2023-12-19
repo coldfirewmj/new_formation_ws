78b2b45dc71f0add6fccfef10d98becbb3d92bdd
wmj <1746508940@qq.com>
2023-12-19 16:34:30 +0800

    增加msg_scoket_bridge

---
+++ src/msg_socket_bridge/include/msg_socket_bridge/TopicEnum.hpp
@@ -0,0 +1,20 @@
+#ifndef __MSG_TYPE_HPP_
+#define __MSG_TYPE_HPP_
+
+
+
+enum TOPIC_ENUM
+{
+  OTHER_ODOM = 10,
+  OTHER_BSPLINE,
+  TAKEOFF,
+  LAND,
+  POINT_CMD,
+  POINTCLOUD,
+  PING_DELAY,
+  EGO_TRIGGER,
+  OTHER_MINCO
+} massage_type_;
+
+
+#endif
